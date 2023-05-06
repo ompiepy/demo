3f1e0636563e46651e563c0408e3c11400013052
Om <om@ompra>
2023-05-06 15:42:58 +0545

    added ci/cd

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Deer Hack 2023' });
+  res.render('index', { title: 'Deer Hack 2023 Sifal' });
 });
 
 module.exports = router;
